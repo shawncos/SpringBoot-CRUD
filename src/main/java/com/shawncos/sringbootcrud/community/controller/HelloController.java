0755780a8f40b368acb138f3ca83edd04a91bd1c
shawncos <shawncoscx@gmail.com>
2020-02-17 20:45:33 +0800

    add bootstrap

--- src/main/java/com/shawncos/sringbootcrud/community/controller/HelloController.java
+++ src/main/java/com/shawncos/sringbootcrud/community/controller/HelloController.java
@@ -1,4 +1,14 @@
 package com.shawncos.sringbootcrud.community.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/")
+    public  String index(){
+        return "index";
+    }
 }
